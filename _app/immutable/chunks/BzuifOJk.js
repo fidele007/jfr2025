37ec30e7602287555011368712c4b3576610653b
Force Fidele KIEN <forcefidele@gmail.com>
2025-10-11 01:05:28 +0200

    Updates

---
+++ _app/immutable/chunks/BzuifOJk.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_r2ovjr?.base??"",a=globalThis.__sveltekit_r2ovjr?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/De-7dWs-.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1v5if5p?.base??"",a=globalThis.__sveltekit_1v5if5p?.assets??s??"";export{a,s as b};
