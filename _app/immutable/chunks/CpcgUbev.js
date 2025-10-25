a6aa11db24c1236e014f726dbad9955db776a9c4
Force Fidele KIEN <forcefidele@gmail.com>
2025-10-25 03:46:08 +0200

    Updates

---
+++ _app/immutable/chunks/CpcgUbev.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1nssiux?.base??"",a=globalThis.__sveltekit_1nssiux?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.B3N2ZZO-.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/C17qICt_.js";export{o as load_css,r as start};
